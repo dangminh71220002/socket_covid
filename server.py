9ea685cd270fd70b107c5192e14e0c1104339fcb
dangminh71220002 <83014500+dangminh71220002@users.noreply.github.com>
2021-07-08 18:10:28 +0700

    Update server.py

--- server.py
+++ server.py
@@ -32,46 +32,51 @@ class FirstScreen(tk.Tk):
         self.resizable(False,False)
         self.first_frame = tk.Frame(self, bg="sky blue")
         self.first_frame.pack(fill="both", expand=True)
+        #set BackGround
         background = Image.open("image/cam.jpg")
         background = background.resize((1200, 600), Image.ANTIALIAS)
         self.background = ImageTk.PhotoImage(background)
         tk.Label(self.first_frame, image=self.background).place(x=0, y=0)
 
 
-
+        #Text area chat of clientS and server
         self.text_area = tk.scrolledtext.ScrolledText(self.first_frame)
-        self.text_area.place(x=275,y=85)
+        self.text_area.place(x=475,y=85)
         self.text_area.config(state='disabled',fg="#00B7FE")
         self.text_area.configure(bg="white")
         
 
         receive_thread = threading.Thread(target = self.receive)
         receive_thread.start()
+        #---------Lable------------------
         tk.Label(self.first_frame,text="SERVER",font=("Impact",20,"bold"),fg="#d77337",bg="#DED461").place(x=570,y=20)
-        tk.Label(self.first_frame,text="Client",font=("times new roman",15),fg="#d77337",bg="#DED461").place(x=93,y=90)
-        tk.Label(self.first_frame,text="Kick Client",font=("times new roman",15),fg="#d77337",bg="#DED461").place(x=1030,y=90)
-        self.button_Kick=tk.Button(self.first_frame,cursor="hand2",text="Kick",fg="white",bg="#d77337",font=("times new roman",15),command=self.writeKick).place(x=1050,y=200,width=60,height=30)
-        
+        tk.Label(self.first_frame,text="Client",font=("times new roman",15),fg="gray",bg="#DED461").place(x=40,y=110)
+        tk.Label(self.first_frame,text="Kick Client",font=("times new roman",15),fg="gray",bg="#DED461").place(x=40,y=370)
+        tk.Label(self.first_frame,text="Chat",font=("times new roman",15),fg="gray",bg="#DED461").place(x=480,y=50)
+        #===================================================
+        #button Kick clients
+        self.button_Kick=tk.Button(self.first_frame,cursor="hand2",text="KICK",fg="white",bg="#d77337",font=("times new roman",15),command=self.writeKick).place(x=200,y=480,width=60,height=30)
+        #text area status of client
         self.text_user = tk.scrolledtext.ScrolledText(self.first_frame)
         self.text_user.config(state='disabled',fg="#00B7FE")
         self.text_user.configure(bg="white")
-        self.text_user.place(x=30,y=136,width=200,height=200)
+        self.text_user.place(x=30,y=136,width=400,height=200)
 
-
+        #Enter name of client server want Kick
         self.input_kick = tk.Text(self.first_frame)
         self.input_kick.config(font=("Transformers Movie",10))
-        self.input_kick.place(x=980, y=136,width=200,height=50)
-
+        self.input_kick.place(x=30, y=400,width=400,height=50)
+        #Enter mess of server to client
         self.input = tk.Text(self.first_frame)
         self.input.config(font=("Transformers Movie",10))
-        self.input.place(x=280, y=500,width=570,height=50)
-        self.send=tk.Button(self.first_frame,cursor="hand2",text="send",fg="white",bg="#d77337",font=("times new roman",15,),command=self.ServerChat).place(x=870,y=500,width=60,height=50)
+        self.input.place(x=475, y=500,width=570,height=50)
+        self.send=tk.Button(self.first_frame,cursor="hand2",text="SEND",fg="white",bg="#d77337",font=("times new roman",15,),command=self.ServerChat).place(x=1070,y=500,width=60,height=50)
         thread2 = threading.Thread(target=self.ServerChat,args=( ))
         thread2.start() 
         
         self.mainloop()
 
-
+#--------------- function of KICK
     def kickClient(self,name):
         if name in nicknames:
             name_index= nicknames.index(name)
@@ -80,7 +85,6 @@ class FirstScreen(tk.Tk):
             client_kick.send("You were kicked by admin ".encode('utf-8'))   
             client_kick.close()
             nicknames.remove(name)
-            print("dang")
         
 
     def writeKick(self):
@@ -90,7 +94,8 @@ class FirstScreen(tk.Tk):
         self.kickClient(name_kick)
         
 
-               
+#================================
+# send mess of server to clients          
     def broadcast(self,message):
         for client in clients:
             client.send(message)
@@ -114,6 +119,7 @@ class FirstScreen(tk.Tk):
                 nickname=nicknames[index]
                 nicknames.remove(nickname)
                 break
+    #mess's server send to client
     def ServerChat(self):
         index=self.input.get('1.0','end')
         if index!='\n':
@@ -155,10 +161,10 @@ class FirstScreen(tk.Tk):
 
 
 
-            self.text_area.config(state='normal')
-            self.text_area.insert('end',f"NICKNAME of  the clients is: {nickname}\n")
-            self.text_area.yview('end')
-            self.text_area.config(state='disabled')
+            self.text_user.config(state='normal')
+            self.text_user.insert('end',f"NICKNAME of  the clients is: {nickname}\n")
+            self.text_user.yview('end')
+            self.text_user.config(state='disabled')
 
 
             self.broadcast(f"{nickname} connected to server \n".encode('utf-8'))
@@ -167,10 +173,10 @@ class FirstScreen(tk.Tk):
             thread1.start()
         else:
             client.send('wrong_pass'.encode('utf-8'))
-            self.text_area.config(state='normal')
-            self.text_area.insert('end',f"{address} disconnected\n")
-            self.text_area.yview('end')
-            self.text_area.config(state='disabled')
+            self.text_user.config(state='normal')
+            self.text_user.insert('end',f"{address} disconnected\n")
+            self.text_user.yview('end')
+            self.text_user.config(state='disabled')
             client.close()
 
     #========register=========
@@ -194,10 +200,10 @@ class FirstScreen(tk.Tk):
             self.text_area.config(state='disabled')
         else:
             client.send('exists'.encode('utf-8'))
-            self.text_area.config(state='normal')
-            self.text_area.insert('end',f"{address} disconnected\n")
-            self.text_area.yview('end')
-            self.text_area.config(state='disabled')
+            self.text_user.config(state='normal')
+            self.text_user.insert('end',f"{address} disconnected\n")
+            self.text_user.yview('end')
+            self.text_user.config(state='disabled')
             client.close()
 
 
@@ -205,10 +211,10 @@ class FirstScreen(tk.Tk):
         while True:
             client,address  =server.accept()
 
-            self.text_area.config(state='normal')
-            self.text_area.insert('end',f"Connected with{str(address)}\n")
-            self.text_area.yview('end')
-            self.text_area.config(state='disabled')
+            self.text_user.config(state='normal')
+            self.text_user.insert('end',f"Connected with{str(address)}\n")
+            self.text_user.yview('end')
+            self.text_user.config(state='disabled')
 
             if len(clients)==5:
                 client.send('not_allowed'.encode())
@@ -222,10 +228,10 @@ class FirstScreen(tk.Tk):
                 option  = client.recv(1024).decode('utf-8')
                 print(option,nickname,password)
             except:
-                self.text_area.config(state='normal')
-                self.text_area.insert('end',f"{address} disconnected\n")
-                self.text_area.yview('end')
-                self.text_area.config(state='disabled')
+                self.text_user.config(state='normal')
+                self.text_user.insert('end',f"{address} disconnected\n")
+                self.text_user.yview('end')
+                self.text_user.config(state='disabled')
                 client.close()
                 continue
             if option=='login': self.ProcessLogin(nickname,password,client,address)
